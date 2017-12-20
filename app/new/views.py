37d44f186b879cc4067655f9b12eaec67f5c76fa
zuoxin <zuoxin2008@126.com>
2017-12-20 14:54:37 +0800

    新闻页

--- app/new/views.py
+++ app/new/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, redirect, url_for, flash
+from flask import render_template, session, redirect, url_for, flash, request
 from flask_login import login_required, current_user
 from . import new
 from ..models import User, Post, New
@@ -14,14 +14,12 @@ def news():
 def push_new():
     form = NewForm()
     if form.validate_on_submit():
-        print('wocao')
         filename = secure_filename(form.image.data.filename)
         form.image.data.save('app/static/upload/' + filename)
         new = New(title=form.title.data,
                   content=form.content.data,
                   author=current_user._get_current_object(),
                   imagefile=secure_filename(form.image.data.filename))
-        print('123')
         try:
             new.save()
             flash('你已经发布成功')
@@ -29,3 +27,8 @@ def push_new():
         except:
             flash("发布失败")
     return render_template('push.html', form=form)
+
+@new.route('/new_show/<newid>', methods=['GET', 'POST'])
+def new_show(newid):
+    newsshow = New.select().where(New.id == newid)
+    return render_template('newshow.html', news=newsshow)
